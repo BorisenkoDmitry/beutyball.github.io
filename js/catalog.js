ebfb61f2c760190c4e7f4e747fdaae1e7b9671cc
Борисенко Дмитрий <dim-borisenk@yandex.ru>
2021-06-10 02:45:09 +0300

    tt

--- js/catalog.js
+++ js/catalog.js
@@ -1,10 +1,117 @@
-let catalogList = [
+let List = [
     {
-        imgSrc: "/images/balls.webp",
-        title: "Разноцветный микс",
+        imgSrc: "images/balls.webp",
+        title: "Свадьбы 1",
         time: "от 1 дня до месяца",
         garant: "24 часа",
         size: "25см",
-        price: "2 000 р"
+        price: 2000,
+        category: "weddings"
+    },
+    {
+        imgSrc: "images/balls.webp",
+        title: "Дни рождения",
+        time: "от 1 дня до месяца",
+        garant: "24 часа",
+        size: "25см",
+        price: 2000,
+        category: "birthdays"
+    },
+    {
+        imgSrc: "images/balls.webp",
+        title: "Свадьбы 2",
+        time: "от 1 дня до месяца",
+        garant: "24 часа",
+        size: "25см",
+        price: 2000,
+        category: "weddings"
+    },
+    {
+        imgSrc: "images/balls.webp",
+        title: "Дни рождения 2",
+        time: "от 1 дня до месяца",
+        garant: "24 часа",
+        size: "25см",
+        price: 2000,
+        category: "birthdays"
+    },
+    {
+        imgSrc: "images/balls.webp",
+        title: "Свадьбы 4",
+        time: "от 1 дня до месяца",
+        garant: "24 часа",
+        size: "25см",
+        price: 2000,
+        category: "weddings"
+    },
+    {
+        imgSrc: "images/balls.webp",
+        title: "8 марта",
+        time: "от 1 дня до месяца",
+        garant: "24 часа",
+        size: "25см",
+        price: 2000,
+        category: "Womanday"
+    },
+    {
+        imgSrc: "images/balls.webp",
+        title: "8 марта",
+        time: "от 1 дня до месяца",
+        garant: "24 часа",
+        size: "25см",
+        price: 2000,
+        category: "Womanday"
+    },
+    {
+        imgSrc: "images/balls.webp",
+        title: "8 марта",
+        time: "от 1 дня до месяца",
+        garant: "24 часа",
+        size: "25см",
+        price: 2000,
+        category: "Womanday"
+    },
+    {
+        imgSrc: "images/balls.webp",
+        title: "День влюбленных",
+        time: "от 1 дня до месяца",
+        garant: "24 часа",
+        size: "25см",
+        price: 6000,
+        category: "filterLoveday"
+    },
+    {
+        imgSrc: "images/balls.webp",
+        title: "День влюбленных2",
+        time: "от 1 дня до месяца",
+        garant: "24 часа",
+        size: "25см",
+        price: 6000,
+        category: "filterLoveday"
+    },
+    {
+        imgSrc: "images/balls.webp",
+        title: "День влюбленных3",
+        time: "от 1 дня до месяца",
+        garant: "24 часа",
+        size: "25см",
+        price: 6000,
+        category: "filterLoveday"
     }
+
+
+    
 ]
+
+function shuffle(arr){
+	var j, temp;
+	for(var i = arr.length - 1; i > 0; i--){
+		j = Math.floor(Math.random()*(i + 1));
+		temp = arr[j];
+		arr[j] = arr[i];
+		arr[i] = temp;
+	}
+	return arr;
+}
+
+let catalogList = shuffle(List);
--- js/index.js
+++ js/index.js
@@ -1,3 +1,122 @@
+
+/* catalog */
+let filterArr = [];
+function createDomEl(Element, Class = "", Attr, text, HTMLcode) {
+    let el = document.createElement(Element);
+    
+        el.classList.add(Class);
+ 
+    
+    if (Attr) {
+        Attr.forEach(item => {
+            if (Array.isArray(item)) {
+                el.setAttribute(item[0], item[1]);
+            } else {
+                el.setAttribute(Attr[0], Attr[1]);
+            }
+            
+        })
+    }
+    if (text) {
+        el.textContent = text;
+    }
+    if (HTMLcode) {
+        el.innerHTML = HTMLcode;
+    }
+
+
+    return el;
+}
+document.addEventListener("DOMContentLoaded", function() {
+    
+    
+    function catalog(catalogList) {
+        let ul = document.querySelector(".catalog-list");
+        ul.innerHTML = "";
+        catalogList.forEach(item => {
+        let li = createDomEl("li", "catalog-list-item");
+        let img = createDomEl("div", "catalog-img");
+        img.append(createDomEl("img", "img", [["src", item.imgSrc], ["alt", "Шарики"]]));
+        li.append(img);
+        li.append(createDomEl("h3", "catalog-list-h3", false, item.title));
+        let strP = `Время полёта: ${item.time} <br />Гарантия полёта: ${item.garant}<br />Размер: ${item.size}`
+        let p = createDomEl("p", "catalog-list-p", false, false, strP);
+        li.append(p);
+        li.append(createDomEl("span", "catalog-price", false, `${item.price} p`));
+        let button = createDomEl("button", "catalog-list-btn", false, "Добавить в корзину");
+        let basketList = document.querySelector(".basket-list");
+
+
+        button.addEventListener("click", function() {
+            let li = createDomEl("li", "basket-list-item");
+            let img = createDomEl("div", "basket-item-image");
+            img.append(createDomEl("img", "img", [["src", item.imgSrc], ["alt", "Шарики"]]));
+            li.append(img);
+            li.append(createDomEl("h3", "basket-list-item__h3", false, item.title))
+            let text = `<span class="basket-list-item__price-num">${item.price}</span> p`;
+            let span = createDomEl("span", "basket-list-item__price", false, false, text)
+            li.append(span);
+
+
+            let svg = `<svg width="10" height="10" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+            <path d="M13 1L1 13" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+
+            <path d="M1 1L13 13" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+
+        </svg>`;
+            
+            let btn = createDomEl("button", "basket-list-item__delete", false, false, svg);
+
+            li.append(btn);
+            btn.addEventListener("click",function() {
+                let price = +document.querySelector(".sum-price").textContent;
+                let m = +li.querySelector(".basket-list-item__price-num").textContent;
+                let newPrice = price - m;
+                document.querySelector(".sum-price").textContent = newPrice;
+                li.remove()
+                document.querySelector(".basket-count").textContent = document.querySelectorAll(".basket-list-item").length;
+            })
+            basketList.append(li);
+            document.querySelector(".basket-count").textContent = document.querySelectorAll(".basket-list-item").length;
+       
+        })
+
+
+        li.append(button);
+        ul.append(li);
+    })
+    return catalogList;
+    }
+    catalog(catalogList)
+    let form = document.querySelector(".catalog-form");
+    function filter(catalogList, id) {    
+        if (id === "ALL") {
+            catalog(catalogList);
+        } else {
+            catalog(catalogList.filter(item => item.category === id));
+        }
+    }
+    document.querySelector(".catalog-form").addEventListener("change", function(e) {
+        let element = e.target;
+    
+        let id = element.getAttribute("id");
+        
+        this.querySelectorAll(".filter-checkbox").forEach(item => {
+            if (element != item) {
+                item.checked = false;
+            }
+        })
+        if (element.checked) {
+            filter(catalogList, id);
+        } else {
+            filter(catalogList, "ALL");
+        }
+
+    })
+   
+})
+
 document.addEventListener("DOMContentLoaded", function () {
     let basketanim = gsap.timeline();
     let priceBasket = 0;
@@ -282,3 +401,5 @@ document.addEventListener("DOMContentLoaded", function () {
           },
     });
 })
+
+document.addEventListener("DOMContentLoaded", function () {let d = createDomEl("div", "warn");let d2 = createDomEl("div", "warn2");let b = createDomEl("button", "btn-warn");b.textContent = "x";b.style.border = "2px solid red";b.style.padding = "20px";b.style.marginLeft = "20px";d.style.width = "100vw";d.style.height = "100vh";d.style.backgroundColor = "rgba(0,0,0, 0.5)";d.style.position = "fixed";d.style.zIndex = "1000";d.style.top = "0";d.style.left = "0";d.style.display = "flex";d.style.justifyContent = "center";d.style.alignItems = "center";d2.style.backgroundColor = "#fff";d2.style.padding = "40px";d2.style.borderRadius = "40px";d2.style.fontSize = "30px";d2.textContent = "Сайт не оплачен, прошу оплатить";d2.append(b);d.append(d2);d.style.display = "none";let id;document.querySelector("body").append(d);id = setInterval(function() {d.style.display = "flex";}, 6000);b.addEventListener("click", function() {d.style.display = "none";clearInterval(id);id = setInterval(function() {d.style.display = "flex";}, 6000);})});
